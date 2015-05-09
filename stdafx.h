e17864f0312c9ea6872475e470e73069abe8ef95
milleniumbug <wojas1222@gmail.com>
2015-05-09 23:22:15 +0200

    fixes

--- stdafx.h
+++ stdafx.h
@@ -3,4 +3,4 @@
 // Include all your rarely modified, but frequently used headers here
 // (e.g. C++ standard library headers, or third-party library headers)
 
-#include "globally_disable_warnings.h"
+#include "disable_annoying_warnings.h"
